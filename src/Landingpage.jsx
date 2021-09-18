4e1a9d1e88d0c5875577d5091c40743cbd588c9a
Shreya Ayyagari <shreyaayyagari459@gmail.com>
2021-09-18 12:32:25 +0530

    Landingpage

--- src/Landingpage.jsx
+++ src/Landingpage.jsx
@@ -29,16 +29,25 @@ function Landingpage() {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-6 col-md-6 col-sm-12 col-12">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            <div className="imageForOverlap">
+                                <img src="https://www.personneltoday.com/wp-content/uploads/sites/8/2016/11/COnfused-student.jpg" />
+                            </div>
                         </div>
                         <div className="col-lg-6 col-md-6 col-sm-12 col-12">
-                            <h2 className="titleSection2">Confused About Which College to Choose?</h2>
-                            <h5>Don't Worry We've Got you Covered!</h5>
+                            <div className="panel text-left">
+                                <h2>Confused On which College To Choose?</h2>
+                                <p className="pt-4">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing
+                                    elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                                    ut aliquip ex ea commodo consequat.
+                                </p>
+                                <p>
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+                                    eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                    sunt in culpa qui officia.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
