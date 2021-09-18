c8b518f5f28f36cb7870ea1ec1eed333a0cd809b
Shreya Ayyagari <shreyaayyagari459@gmail.com>
2021-09-18 17:18:38 +0530

    Light changes
    Felt like we could keep it register since the default sign is the signup. I hope you don't mind that i changed it back

--- src/Header.js
+++ src/Header.js
@@ -5,34 +5,29 @@ import Container from "react-bootstrap/Container";
 import "./Header.css";
 
 function Header() {
-  return (
-    <div>
-      <Navbar variant="dark" expand="xl" className="colorNav">
-        <Container>
-          <Navbar.Brand href="#home" style={{ color: "#E8D454" }}>
-            React-Bootstrap
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home" style={{ color: "#E8D454" }}>
-                Home
-              </Nav.Link>
-              <Nav.Link href="#link" style={{ color: "#E8D454" }}>
-                About Us
-              </Nav.Link>
-              <Nav.Link href="/login" style={{ color: "#E8D454" }}>
-                Sign in/up
-              </Nav.Link>
-              <Nav.Link href="#link" style={{ color: "#E8D454" }}>
-                Contact Us
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar variant="dark" expand="xl" className="colorNav">
+                <Container>
+
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link className="navLink" href="#home">
+                                Home
+                            </Nav.Link>
+                            <Nav.Link className="navLink" href="/login">
+                                Register
+                            </Nav.Link>
+                            <Nav.Link className="navLink" href="#link">
+                                Contact Us
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
 }
 
 export default Header;
